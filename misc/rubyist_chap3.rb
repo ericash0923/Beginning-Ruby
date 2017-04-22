7a14063e40c38c36a20807ec03802e153b7eed85
Eric Ash <eric@gaglani.com>
2017-04-22 15:58:20 -0400

    ruby monk

---
+++ misc/rubyist_chap3.rb
@@ -0,0 +1,231 @@
+### Define a class
+
+class Ticket
+  def event
+    "Can't really be specified yet..."
+  end
+end
+
+#create a new object with Ticket method
+ticket = Ticket.new
+#method call ticket.event execution
+puts ticket.event
+
+
+## Listing 3.1
+## An instance variable maintaining its value between method calls
+
+class Person
+  def set_name(string)
+    puts "Setting person's name...."
+    @name = string
+  end
+
+## instance variable start with @
+  def get_name
+    puts "Returning the person's name..."
+    @name
+  end
+end
+
+# create Person object to joe
+joe = Person.new
+# Setting his name
+joe.set_name("Joe")
+# Retrieve the person name
+puts joe.get_name
+
+## Initializing an object with state
+
+## see the message "Creating a new ticket!" every time you create a new ticket
+## object by calling Ticket.new.
+class Ticket
+  def initialize
+    puts "Creating a new ticket"
+  end
+end
+
+Ticket.new
+
+#### Dynamically set vaules
+class Ticket
+  ## have access to the venue and date information and store by instance variables
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+
+  ## read back venue
+  def venue
+    @venue
+  end
+
+  ## read back date
+  def date
+    @date
+  end
+end
+
+th = Ticket.new("Town Hall", "11/12/13")
+cc = Ticket.new("Convention Center", "12/13/14")
+puts "We've created two tickets"
+puts "The first is for a #{th.venue} event on #{th.date}"
+puts "The second is for a #{cc.venue} event on #{cc.date}"
+
+# We've created two tickets
+# The first is for a Town Hall event on 11/12/13
+# The second is for a Convention Center event on 12/13/14
+
+##### 3.3 Setter methoods
+
+class Ticket
+  ## have access to the venue and date information and store by instance variables
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+
+  ## allow us to set or reset the price of an existing ticket
+  ## setter method
+  def set_price (amount)
+    @price = amount
+  end
+
+  ## read back venue
+  def venue
+    @venue
+  end
+
+  ## read back date
+  def date
+    @date
+  end
+
+  def price
+    @price
+  end
+end
+
+ticket = Ticket.new("Town Hall", "11/12/13")
+ticket.set_price(63.00)
+puts "The ticket cost $#{"%.2f" % ticket.price}."  ## Format price to two decimal places
+ticket.set_price(72.50)
+puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price}."
+
+
+### Attributes and the attr_* method family
+
+## More compact version
+
+class Ticket
+  attr_reader :venue, :date, :price
+  attr_writer :price
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+end
+
+## Long Version
+
+class Ticket
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+
+  ## allow us to set or reset the price of an existing ticket
+  ## setter method (writer)
+  def price=(price)
+    @price = price
+  end
+
+  ## read back venue (reader)
+  def venue
+    @venue
+  end
+
+  ## read back date (reader)
+  def date
+    @date
+  end
+
+  ## read back price (reader)
+  def price
+    @price
+  end
+end
+
+## CREATING READER/WRITER ATTRIBUTES WITH ATTR_ACCESSOR
+
+class Ticket
+  attr_reader :venue, :date
+  attr_accessor :price ##combine attr_reader and attr_writer
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+end
+
+## Inheritance and the Ruby class hierarchy
+
+class C
+end
+
+class D < C
+end
+
+puts D.superclass
+puts D.superclass.superclass
+
+### Output
+### C
+### Object
+### C is D's superclass and Object is C's superclass
+
+### #3.6.3 A singleton method by any other name…
+
+class Ticket
+  attr_reader :venue, :date
+  attr_accessor :price ##combine attr_reader and attr_writer
+  def initialize (venue,date)
+    @venue = venue
+    @date = date
+  end
+
+  def Ticket.most_expensive(*tickets)
+    tickets.max_by(&:price)
+  end
+
+end
+
+## method most_expensive is defined directly on the class object Ticket, in singleton-method style
+## Message most_expensive goes to the class Ticket, not to a particular ticket
+
+th = Ticket.new("Town Hall","11/12/13")
+cc = Ticket.new("Convention Center","12/13/14/")
+fg = Ticket.new("Fairgrounds", "13/14/15/")
+th.price = 12.55
+cc.price = 10.00
+fg.price = 18.00
+highest = Ticket.most_expensive(th,cc,fg)
+puts "The highest-priced ticket is the one for #{highest.venue}."
+
+## Output
+## The highest-priced ticket is the one for Fairgrounds.
+
+
+### #3.6.4 When, and why, to write a class method
+class Temperature
+  def Temperature.c2f(celsius)
+    celsius * 9.0 / 5 + 32
+  end
+  def Temperature.f2c(fahrenheit)
+    (fahrenheit - 32) * 5 / 9.0
+  end
+end
+
+puts Temperature.c2f(100)
+
+## practically speaking, having a Temperature class with class
+## methods to perform the conversions is adequate and is an acceptable design
--- misc/rubymonk.rb
+++ misc/rubymonk.rb
@@ -90,3 +90,105 @@ end
 
 sum_of_cubes(1,3)
 sum_of_cubes(3,5)
+
+######################################
+# Find non-duplicate values in an Array
+######################################
+
+def non_duplicated_values(values)
+  values.each do |x|
+    values.delete(x) if values.count(x) == 2
+  end
+end
+
+non_duplicated_values([1,2,2,3,3,4,5])
+non_duplicated_values([1,2,2,3,4,4])
+
+# def non_duplicated_values(values)
+# find values with count only equal to 1 remove any copys
+#   values.find_all { |x| values.count(x) == 1 }
+# end
+
+############################################
+Check if all elements in an array are Fixnum
+############################################
+# def array_of_fixnums?(array)
+# array.each do |x|
+#   if x.class != Fixnum
+#     return false
+#   else
+#     true
+#   end
+# end
+
+def array_of_fixnums?(array)
+  array.all? { |x| x.is_a? Fixnum }
+end
+
+array_of_fixnums([1,2,3])
+array_of_fixnums(['a',2,'b'])
+
+
+############################################
+## Kaprekar's Number
+############################################
+
+# Kaprekar number k with n-digits, if you square it and add the
+# right n digits to the left n or n-1 digits, the resultant sum is k
+
+def kaprekar?(k)
+  # k was convert to string to use the array properties
+  num_length = k.to_s.size
+  square = (k ** 2).to_s
+
+  second_half = square[-num_length..-1]
+  #if number is even n-1; odd number n-2
+  first_half = square.size.even? ? square[0..no_of_digits-1] : square[0..no_of_digits-2]
+
+  # must convert all number back to integer since it was convert
+  # to string
+  k == first_half.to_i + second_half.to_i
+end
+
+kaprekar?(9)
+kaprekar?(55)
+kaprekar?(297)
+kaprekar?(703)
+
+############################################
+Enough Contrast?
+############################################
+
+class Color
+  attr_reader :r, :g, :b
+  def initialize(r, g, b)
+    @r = r
+    @g = g
+    @b = b
+  end
+
+  def brightness_index
+    (r*299 + g*587 + b*114) / 1000
+  end
+
+  ## Absolute difference in brighness indices
+  def brightness_difference(another_color)
+    ##two object -- 2 colors
+    (brightness_index - another_color.brightness_index).abs
+  end
+
+
+  def hue_difference(another_color)
+    #|R1 - R2| + |G1 - G2| + |B1 - B2|
+    (r - another_color.r).abs +
+    (g - another_color.g).abs +
+    (b - another_color.b).abs
+  end
+
+  ## Brightness difference is more than 125 and the Hue difference
+  ## is more than 500 then the colors have sufficient contrast
+  def enough_contrast?(another_color)
+    brightness_difference(another_color) > 125 &&
+    hue_difference(another_color) > 500
+  end
+end
